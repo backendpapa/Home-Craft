1deda4e1af55a54a33496892d510697d7da7781a
Akeju Paul <akejupaul10@gmail.com>
2021-09-29 18:11:54 +0100

    make

---
+++ screens/DetailsScreen.vue
@@ -0,0 +1,15 @@
+<template>
+<view>
+    <text class="text-color-primary">Home</text>
+</view>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ screens/HomeScreen.vue
@@ -0,0 +1,23 @@
+<template>
+  <view class="container">
+    <text class="">Home</text>
+</view>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+.container {
+  background-color: white;
+  align-items: center;
+  justify-content: center;
+  flex: 1;
+}
+.text-color-primary {
+  color: blue;
+}
+</style>
